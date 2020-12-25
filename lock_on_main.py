f766f50d49d923f1f76da2567161b338747c69d2
Morteza Noshad <mrtnoshad@users.noreply.github.com>
2020-12-24 19:31:46 -0800

    Update lock_on_main.py

--- lock_on_main.py
+++ lock_on_main.py
@@ -2,6 +2,6 @@ import time
 import os 
 
 while True:
-	time.sleep(4)
+	time.sleep(1)
 	#print('lock')
 	os.popen('if test $(wmctrl -l | grep "Wi-Fi Network Authentication Required" 2>&1 | wc -l) -eq 1; then wmctrl -a "Wi-Fi Network Authentication Required"; elif test $(wmctrl -l | grep "Network" 2>&1 | wc -l) -eq 1; then wmctrl -a "Network"; else wmctrl -a "Shyld AI"; fi')
