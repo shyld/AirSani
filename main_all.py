8ce30212e89481d7cc5c2c85b8cf5d06ab869d91
Morteza Noshad <mrtnoshad@users.noreply.github.com>
2020-12-24 17:40:37 -0800

    Update main_all.py

--- main_all.py
+++ main_all.py
@@ -23,6 +23,7 @@ import subprocess
 from subprocess import Popen
 import sys
 
+Popen(["unity-control-center network"], shell=True)
 #subprocess.Popen(["python3", "/home/shyldai/shyld/AirSani/lock_on_main.py"])
 
 # insert at 1, 0 is the script path (or '' in REPL)
